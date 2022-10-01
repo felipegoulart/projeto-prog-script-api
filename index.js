a0e5ef07579201cb6b7e09e601732ee490c5cc5f
Luis Felipe Goulart <luisfelipegoulart94@gmail.com>
2022-10-01 16:48:47 -0300

    Init API routes

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+app.get('/', (req, res) => res.json({ message: 'Hello' }))
+
+app.listen(PORT, () => console.log('Running'))
