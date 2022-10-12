c6fd81403393bf5568f000e30aa959973b208248
Luis Felipe Goulart <luisfelipegoulart94@gmail.com>
2022-10-12 15:00:32 -0300

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import cors from 'cors'
 dotenv.config()
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 let empregados = []
 
